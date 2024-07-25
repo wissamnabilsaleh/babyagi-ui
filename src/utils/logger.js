5f3ff2ce36be3760a339a62c67ab6cdafc3df104
wissamnabilsaleh <119110416+wissamnabilsaleh@users.noreply.github.com>
2024-07-25 15:40:19 +0400

    Initial working

---
+++ src/utils/logger.js
@@ -0,0 +1,17 @@
+const { createLogger, transports, format } = require('winston');
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level}]: ${message}`;
+    })
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: 'combined.log' })
+  ],
+});
+
+module.exports = logger;
